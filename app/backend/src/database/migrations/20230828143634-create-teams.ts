624ba84b037838b4466c25af0a20f561defbf502
EmersonMarinho <Emerson_marinhoaz@hotmail.com>
2023-08-28 12:04:33 -0300

    feat: created the teams models and also the migrations and verified if it works

---
+++ app/backend/src/database/migrations/20230828143634-create-teams.ts
@@ -0,0 +1,20 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import ITeam from '../../Interfaces/ITeam'
+
+export default {
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    await queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'team_name',
+      },
+    });
+  }
+} 
